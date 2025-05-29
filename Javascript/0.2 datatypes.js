3b1b2d19bf32228f653c367f920b9cb5b37e33a7
Tejaswini <tejaswinip972002@gmail.com>
2025-05-29 15:05:22 +0530

    Adding Javascript datatypes program

---
+++ Javascript/0.2 datatypes.js
@@ -0,0 +1,31 @@
+//number//
+
+let a= 99.6;
+let price =45;
+console.log(typeof a);
+
+//string//
+
+let fullname= "Teju Patil";
+//boolean//
+
+let isonline= true;
+let isadmin= false;
+console.log (typeof isonline);
+//undefined//
+let x;
+console.log(x);
+
+//BigInt//
+let y= BigInt(9999);
+console.log(y);
+
+//symbol//
+let z= Symbol("hii");
+//object//
+ const student={
+    name:"Teju Patil",
+    age:22,
+    cgpa:8.2,
+    ispass: true,
+ };
