a2bafeeaa0427b9d7c6d836b7dbb61084eb2f6db
Tejaswini <tejaswinip972002@gmail.com>
2025-08-02 11:20:44 +0530

    Adding program

---
+++ Javascript/String/0.4 Template literal.js
@@ -0,0 +1,8 @@
+
+let student = {
+    name : "teju",
+    age : 21,
+    acadmicyear : 2 ,
+};
+let result = `The student name is ${student.name} and thir age is ${student.age}.` ;
+console.log(result);
