c31a543600458666ac10d6db44d8a0fdae077626
Tejaswini <tejaswinip972002@gmail.com>
2025-08-22 07:43:55 +0530

    Adding Program

---
+++ Javascript/String/0.5 Practise.js
@@ -0,0 +1,11 @@
+// uppercase     // two output yenar 
+let str = "Teju";
+let newstr = str.toUpperCase();
+console.log(str);     // Teju
+console.log(newstr);                     //TEJU
+
+// one output sathii
+
+let str1 ="Patil";
+str1 = str1.toUpperCase();
+console.log(str1);
