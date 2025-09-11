a7e74299dd6158bbbb7811f1a6bfad39830b2fd2
Tejaswini <tejaswinip972002@gmail.com>
2025-09-11 14:03:54 +0530

    Adding program

---
+++ Javascript/Array/0.2 Practise.js
@@ -0,0 +1,14 @@
+
+let student = [
+    {
+        name : "Teju",
+        rollno : 22,
+        ispass : "pass",
+    },
+    {
+        name : "Neha",
+        rollno: 23,
+        ispass : "pass",
+    }
+]
+console.log(student) ;
