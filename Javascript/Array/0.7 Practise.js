09508b93418aba5fca5cdf9b6b115254ecea6a1e
Tejaswini <tejaswinip972002@gmail.com>
2025-09-17 22:24:57 +0530

    Adding Program

---
+++ Javascript/Array/0.7 Practise.js
@@ -0,0 +1,27 @@
+
+let  items = [250,645,300,900,50];
+
+let i= 0;
+for (let val of items){
+    console.log(`val at index ${i}=${val}`);
+    let offer =val/10;
+    items[i]=items[i]-offer;
+    console.log(`value after offer =${items[i]}`)
+    i++;
+}
+
+
+ 
+
+
+
+
+
+/*
+for (let i=0; i<items.length;i++)
+{
+ let offer =items[i]/10;
+ items[i]-= offer;
+ }
+ console.log(items);
+   */
