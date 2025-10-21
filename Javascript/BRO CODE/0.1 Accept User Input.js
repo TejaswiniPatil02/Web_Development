378c8434ec55480554fdb5e116851315b29b7d80
Tejaswini <tejaswinip972002@gmail.com>
2025-10-21 13:46:52 +0530

    Adding Program

---
+++ Javascript/BRO CODE/0.1 Accept User Input.js
@@ -0,0 +1,12 @@
+     //                      ACCEPT USER INPUT /////
+ 
+ 
+ 
+ 
+ 
+ //////////EASY Way ---Window prompt
+let username ;
+
+username = window.prompt("Whats your username???");
+
+console.log(username);
