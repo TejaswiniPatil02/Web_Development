38356c8572822ec3bb7c09ecebf4e7b48f341f0b
Tejaswini <tejaswinip972002@gmail.com>
2025-12-08 22:29:57 +0530

    Adding Program

---
+++ Javascript/BRO CODE/index.js
@@ -0,0 +1,28 @@
+
+//console.log('I like pizza');
+
+document.getElementById("myh1").textContent = 'Hello';
+document.getElementById("myp").textContent = 'I like pizza ';
+
+
+ /*let age = 25;
+ let price = 99.98;
+ let gpa = 7.7
+
+ let email = "tejaswinip972002@gmail.com";
+
+console.log(typeof age);
+console.log(`you are $ {age} years old`);
+
+console.log(`This is my email ${email}`); */
+
+let fullname = "Tejaswini patil";
+let age = 24;
+let student = false ;
+
+document.getElementById("p1").textContent= fullname;
+document.getElementById("p2").textContent = age ;
+document.getElementById("p3").textContent = student;
+
+
+
