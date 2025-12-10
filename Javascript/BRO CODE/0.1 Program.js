80c3045e40dae81325f3d3ce4f52c94851c5e463
Tejaswini <tejaswinip972002@gmail.com>
2025-12-10 21:42:58 +0530

    Adding Program

---
+++ Javascript/BRO CODE/0.1 Program.js
@@ -0,0 +1,14 @@
+
+let student = 30 ;
+/*student = student + 1 ;
+student = student - 1 ;
+student = student / 2 ;
+student = student ** 4 ; */
+
+/* student ++;
+student += 1 ;
+console.log (student); */
+
+let result = 1+ 2 * 3 + 4 ** 2 ;
+
+console.log (result) ;     
