456ac2938c3be2c055bdadf69934f08f2d0752ff
Tejaswini <tejaswinip972002@gmail.com>
2026-04-18 13:37:30 +0530

    Adding Event folder

---
+++ Javascript/Event/0.1 program.js
@@ -0,0 +1,6 @@
+
+let button = document.getElementById("btn")
+
+button.addEventListener("click", ()=>{
+   document.querySelector(".box").innerHTML = "<b> Yayy You were Clicked </b> Enjoy Your click !!!"
+})
