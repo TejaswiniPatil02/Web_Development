ea6af9539611c1ca532669db92b8ca37bd76f99f
Tejaswini <tejaswinip972002@gmail.com>
2026-05-07 15:33:10 +0530

    Adding Program in javascript

---
+++ Javascript/BRO CODE/0.3Program.js
@@ -0,0 +1,47 @@
+
+//let var const fifference
+const accountId = 144553
+let accountEmail = "tejup@9797"
+var accountPassword = "12345"
+accountCity = "Pune"
+
+//convarsion 
+let value = 3;
+let negvalue =-value;
+console.log(negvalue);
+
+accountEmail ="hc@1234"
+accountPassword = "9797"
+accountCity ="Kolhapur"
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity])
+
+console.log(typeof 123)
+
+
+let str1 ="Hello";
+let str2 = " Teju";
+let str3 = str1 + str2
+console.log( str3)
+
+
+//heap or stack
+
+let myyoutube ="tejupatil.com"
+let anothername =  myyoutube
+ myyoutube = "chaiorcode"
+
+ console.log(myyoutube);
+ console.log(anothername);
+
+ let userone = {
+    email : "user@gmail.com",
+    upi : "user@bpi"
+ }
+ let usertwo =userone;
+ 
+ usertwo.email ="teju@gmail.com"
+ console.log(userone.email);
+ console.log(usertwo.email);
+
