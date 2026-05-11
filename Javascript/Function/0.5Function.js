115f9443999bef1f8daf6f093919a7cc6ead06c9
Tejaswini <tejaswinip972002@gmail.com>
2026-05-11 20:53:55 +0530

    Adding Funtion concept in program

---
+++ Javascript/Function/0.5Function.js
@@ -0,0 +1,51 @@
+
+let a= 10;
+let b= 20;
+
+function sum(a,b){
+    console.log(a+b);
+}
+sum(a,b)
+sum(100,200)
+sum(1000,2000)
+
+
+/*function print(){
+    console.log("suman")
+}
+print()*/
+
+
+function substract(a,b){
+    return a-b
+}
+let result = substract(10,20);
+
+console.log(result);
+
+//
+
+function multiply(c,d){
+  return c*d
+}
+let result= multiply(2,4);
+console.log(result);
+
+//
+function greet(name){
+    return`Hello,${name}!`
+
+    let result =greet
+}
+console.log(greet("suman"));
+
+
+function Name(){
+    console.log("T");
+    console.log("E");
+    console.log("J");
+    console.log("U");
+}
+Name()
+
+
